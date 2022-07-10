acdfcf7506834adeece9d7ea452987bbe594a02e
Mizanur Alrasid <88093931+mizanuralrasid@users.noreply.github.com>
2022-07-10 23:52:18 +0530

    Add files via upload

---
+++ include/include.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+
+        <a href="#" class="logo">LOGO</a>
+        
+        <div id="menu-bar" class="fas fa-bars"></div>
+        
+        <nav class="navbar">
+            <a style="--i:0;" href="#home">home</a>
+            <a style="--i:1;" href="#feature">feature</a>
+            <a style="--i:2;" href="services.html">service</a>
+            <a style="--i:3;" href="#project">project</a>
+            <a style="--i:4;" href="#team">team</a>
+            <a style="--i:5;" href="#price">price</a>
+            <a style="--i:6;" href="#contact">contact</a>
+            <a style="--i:7;" href="#registration">Registration</a>
+            <a style="--i:8;" href="#login">login</a>
+        </nav>
+        
+        </header>
+</body>
+</html>
